48878194a09cbb53c0f3f52a85df7caae7102d0a
jwcollins3 <jwcollins3@crimson.ua.edu>
2024-03-02 23:51:28 -0600

    fixed json + facility_type

--- client/resources/scripts/index.js
+++ client/resources/scripts/index.js
@@ -2,24 +2,23 @@
 const map = L.map('map').setView([37.8, -96], 4);
 const filterSelect = document.getElementById('filter');
 
-fetch('/client/resources/data.json')
+fetch('resources/scripts/data.json')
     .then(response => response.json())
     .then(data => {
         data.forEach(item => {
             L.marker([item.latitude, item.longitude]).addTo(map)
-                .bindPopup(item.full_address); // Assuming your JSON has latitude, longitude, and full_address fields
+                .bindPopup(
+                    `<b>${item.facility_name}</b><br>${item.full_address}<br>${item.emr_name}<br>${item.division_name}`
+                    ); // Assuming your JSON has latitude, longitude, and full_address fields
         });
         console.log(data);
     })
     .catch(error => console.error('Error loading data:', error));
  
-
 // Add the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
 }).addTo(map);
-
-
 
 // const filterTypes = [
 //     { name: 'EMR', options: ['EMR', 'EMR Option 1', 'EMR Option 2', 'EMR Option 3'] },
@@ -29,6 +28,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //     { name: 'Address', options: ['Address', 'Address Option 1', 'Address Option 2', 'Address Option 3'] }
    
 // ]
+
 function populateDropdown(data, category, dropdownId) {
     const dropdown = document.getElementById(dropdownId);
     const uniqueValues = [...new Set(data.map(item => item[category]))];
